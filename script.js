71700159c522894a54923c731eea6cb1e652bc65
Kirill Borun <kbroun@yandex.by>
2020-08-25 00:50:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+function number(){
+    //let what = Math.floor(Math.random() * 10);
+    let q = prompt('Угадай число от 1 до 100');
+    if (isNaN(q) || q.trim() === '' || q === null){
+        return number();
+    }
+    
+    function terms(x){
+         if (q>x){
+            alert('Загаданное число меньше') ;
+            return number();}
+         if (q<x){
+         alert('Загаданное число больше');
+         return number();}
+        
+         if (q == x){
+         alert('Вы угадали!');}
+         
+        
+    }
+    terms(55);
+}
+number();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
